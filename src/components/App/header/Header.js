64eb5db3efc412248a9a16b2daa6f525ebf74e80
Eve Stockstill <evestockstillpdx@gmail.com>
2020-05-14 19:38:45 -0700

    init setup done

---
+++ src/components/App/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <>
+      <header className='header'>
+        <h1 className='header__title'>
+          David E. Logue 
+        </h1>
+    <h1 className='title-sub'>Construction</h1>
+      </header>
+    </>
+  );
+};
+export default Header;
