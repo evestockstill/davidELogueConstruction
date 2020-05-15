bba4fd77a5743866cb1b081293a220eb38453bc2
Eve Stockstill <evestockstillpdx@gmail.com>
2020-05-15 03:13:51 -0700

    header outline

---
+++ src/components/header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.scss';
+import Media from 'react-media';
+
+const Header = () => {
+  return (
+    <>
+      <nav className='navbar'>
+        hello
+        <header className='header'>
+          <main className='header--title'>
+            <h1 className='header__title--1'>David E. Logue</h1>
+            <h1 className='header__title--2'>Construction</h1>
+          </main>
+        </header>
+      </nav>
+    </>
+  );
+};
+export default Header;
