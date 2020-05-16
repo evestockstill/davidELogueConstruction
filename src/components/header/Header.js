0cf20e75632b2f9e1ab0c6163fb57b828517de3f
Eve Stockstill <evestockstillpdx@gmail.com>
2020-05-16 03:50:20 -0700

    mixin for text

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import './Header.scss';
-import Media from 'react-media';
+import './_Header.scss';
+import image1 from '../assets/hero/kitchen1.png';
+// import CarouselImage from '..//carouselImage/CarouselImage';
+// import Media from 'react-media';
 
 const Header = () => {
   return (
@@ -11,6 +13,7 @@ const Header = () => {
           <main className='header--title'>
             <h1 className='header__title--1'>David E. Logue</h1>
             <h1 className='header__title--2'>Construction</h1>
+            <img src={image1} className='hero-header' />
           </main>
         </header>
       </nav>
