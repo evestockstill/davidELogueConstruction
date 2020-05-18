727f81d5bc11be886b92fcc6ae4a977713183502
Eve Stockstill <evestockstillpdx@gmail.com>
2020-05-18 14:58:32 -0700

    hero set up

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,21 +1,20 @@
+/* eslint-disable react/jsx-key */
 import React, { useState } from 'react';
 import './_Slider.scss';
 import SliderImg from './SliderImg';
-// import img1 from '../assets/hero/kitchen1.png';
+// import img1 from 'https://ik.imagekit.io/j5m10vikgh/deck1_m1ems6SSz.png';
 // import img2 from '../assets/hero/bathroom.png';
 // import img3 from '../assets/hero/deck.png';
 
 
 const Slider = () => {
   let sliderArr = [
-    // 1, 2, 3
     <SliderImg src='https://ik.imagekit.io/j5m10vikgh/deck1_m1ems6SSz.png'/>,
     <SliderImg src='https://ik.imagekit.io/j5m10vikgh/kitchen1_Mbx4OAgDG.png'/>,
     <SliderImg src='https://ik.imagekit.io/j5m10vikgh/bathroom1_APVNsOz9a.png' />
   ];
-
   const [x, setX] = useState(0);
-
+  
   const handlePrev = () => {
     x === 0 ? setX(-100 * sliderArr.length - 1) : setX(x + 100);
   };
