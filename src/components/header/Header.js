cbb73971d42e9f250554cee0ca2340725363d99f
Eve Stockstill <evestockstillpdx@gmail.com>
2020-05-18 18:46:47 -0700

    check for webclip

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,10 +8,10 @@ const Header = () => {
     <>
       <header className='header'>
         <main className='header--title'>
-          <h3>David E. Logue </h3>
-          <h3>Construction</h3>
-          {/* <h1 className='header__title--1'>David E. Logue</h1>
-          <h1 className='header__title--2'>Construction</h1> */}
+          {/* <h3>David E. Logue </h3>
+          <h3>Construction</h3> */}
+          <h1 className='header__title--1'>David E. Logue</h1>
+          <h2 className='header__title--2'>Construction</h2>
         </main>
       </header>
       {/* <Slider /> */}
